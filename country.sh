99aa0b02f93bb7d9f6c6cab46473e4df4d2b5c37
Fernando Balderas Guzmán <fernando.bguzman@gmail.com>
2022-06-24 16:36:23 +0300

    Add assigment 3

---
+++ country.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+total_args=$#
+
+if [[ $total_args -lt 1 ]]; then
+cat <<EOF
+usage: $0 <country> [<country> ...]
+
+    country   One or more country names separated by spaces
+EOF
+exit 1
+fi
+
+while [ $# -gt 0 ]; do
+    country=$1
+    url="https://restcountries.com/v3.1/name/${country}"
+    
+    response=$(curl --write-out '%{http_code}' --silent --output /dev/null $url)
+    [[ $response -ne 200 ]] && echo "Error: ${country}"
+    
+    if [[ $response -eq 200 ]]; then
+        curl -s $url | jq -r '.[] | {Name: .name?.common?, Capital: (.capital? // [] | join(", ")?), Population: .population?, Languages: (.languages? // [] | join(", ")?), Currency: ([.currencies[]?.symbol?, .currencies[]?.name?] | join(" ")?) } | to_entries[] | "\(.key):\t\(.value)"'
+    fi
+    shift
+    [[ $# -gt 0 ]] && echo
+done
+
